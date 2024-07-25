0ee117dadaea8349e56459951f2a75df7d35a8bd
InnaRomanova <RomanovaInnaP@yandex.ru>
2024-07-25 10:31:38 +0300

    package.json

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Recipe from './pages/Recipe';
 function App() {
   return (
     <> 
-    <Router>
+    <Router basename='/food-projeckt'>
       <Header />
       <main className='container content'>
        
